3b0c7eff90c856f2bb134d168d1ef6f63b5636af
Bas Steins <sebst@users.noreply.github.com>
2024-10-01 14:56:45 +0200

    feat(`pkgx`): Add shell integration.

--- src/features/pkgx/install.sh
+++ src/features/pkgx/install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env -S bash --noprofile --norc -o errexit -o pipefail -o noclobber -o nounset
 
+# From `curl -Ssf https://pkgx.sh`
 ./pkgx-installer.sh
+
+pkgx --integrate && echo "pkgx integrated"
